af9c10d9de128c002bd9df996adf226457f5f837
Cleberton Francisco <clebertonfgc@gmail.com>
2021-07-16 22:57:12 -0300

    Finalizando matrizes

--- vectors-and-matrices.js
+++ vectors-and-matrices.js
@@ -4,3 +4,13 @@ const arrayNumbers = [1, 2, 3, 4 ,5];
 
 arrayNames.map((name) => console.log(name));
 arrayNumbers.map((number) => console.log(number));
+
+// matriz
+
+const matrizNumbers = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+
+for(let line = 0; line < matrizNumbers.length; line ++){
+    for(let column = 0; column < matrizNumbers.length; column ++){
+        console.log(matrizNumbers[line] [column])
+    }
+}
