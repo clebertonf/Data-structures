28fa8c9e1b335af41d6a5cd39ba35f62be220922
Cleberton Francisco <clebertonfgc@gmail.com>
2021-07-16 22:18:56 -0300

    Array names and numbers

--- vetores-matrizes.js
+++ vetores-matrizes.js
@@ -1,2 +1,6 @@
 // array
 const arrayNames = ['Cleberton', 'Katrine', 'Marta', 'Edilson'];
+const arrayNumbers = [1, 2, 3, 4 ,5];
+
+arrayNames.map((name) => console.log(name));
+arrayNumbers.map((number) => console.log(number));
