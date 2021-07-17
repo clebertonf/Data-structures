3c54ca8ee1cb3dc0d4bb11c0de7ee20234b54139
Cleberton Francisco <clebertonfgc@gmail.com>
2021-07-17 09:17:14 -0300

    Renomeando variaveis

--- 03-lists.cs
+++ 03-lists.cs
@@ -2,21 +2,21 @@
 // Codigo precisa esta contido em uma classe e namesapace
 // feito apenas para entendimento dos coneitos de lista
 
-List<int> valores = new List<int>();
+List<int> values = new List<int>();
 
 List<int> range = new List<int>();
 
 for (var i = 0; i <= 10; i++)
 {
-    Random ale = new Random();
+    Random rand = new Random();
 
-    int aleatorio = ale.Next(0, 10);
-    range.Add(aleatorio);
+    int random = rand.Next(0, 10);
+    range.Add(random);
 }
 
-valores.AddRange(range);
-valores.Sort();
-foreach (var item in valores)
+values.AddRange(range);
+values.Sort();
+foreach (var item in values)
 {
     Console.WriteLine(item);
 }
